c99dba1a11a3e4c081ae97492241ceefdca64694
lwilliams1990 <51878981+lwilliams1990@users.noreply.github.com>
2019-08-07 13:26:41 +0100

    Update map_repository.ps1

--- scripts/map_repository.ps1
+++ scripts/map_repository.ps1
@@ -1,2 +1,2 @@
 $net = new-object -ComObject WScript.Network
-$net.MapNetworkDrive("r:", "\\university.ds.derby.ac.uk\private$\DeadlineRepository10", $false, "UNIVERSITY\SRV-DeadlineSlave", "HzL7!TEnM#i8QjLz44F6")
+$net.MapNetworkDrive("r:", "\\university.ds.derby.ac.uk\private$\DeadlineRepository10", $false, "UNIVERSITY\SRV-DeadlineSlave", "PASSWORD")
